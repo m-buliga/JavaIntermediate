9700cdb7303dc22eb9051e433288d81ff55e786c
m-buliga <monica.buliga@endava.com>
2025-08-20 13:22:27 +0300

    practice fluent wait

--- src/test/java/Pages/AlertsPage.java
+++ src/test/java/Pages/AlertsPage.java
@@ -2,11 +2,13 @@ package Pages;
 
 import Logger.LoggerUtility;
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import java.time.Duration;
 
 
@@ -37,16 +39,19 @@ public class AlertsPage extends CommonPage {
         LoggerUtility.infoLog("The user clicked OK button on 1st alert.");
     }
 
-    public void explicitAlertsWait() {
-        // definim un wait explicit care asteapta dupa alerta
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    public void fluentAlertsWait() {
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(2))
+                .ignoring(NoAlertPresentException.class);
+
         wait.until(ExpectedConditions.alertIsPresent());
     }
 
     public void interactWithDelayAlert() {
         elementsMethods.waitForVisibilityOfElement(alertDelayElement);
         elementsMethods.clickElement(alertDelayElement);
-        explicitAlertsWait();
+        fluentAlertsWait();
         Alert alertDelay = driver.switchTo().alert();
         alertDelay.accept();
         LoggerUtility.infoLog("The user clicked OK button on delayed alert.");
